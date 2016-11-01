f8079a7bfc6d750fb99f0b7d9bffc8f90749214e
Jotto0118 <jpotratz7338@stu.d214.org>
2016-11-01 13:54:01 -0500

    added random try code
    learn ui dynamics

--- Haxball/Haxball/ViewController.swift
+++ Haxball/Haxball/ViewController.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+//import CDJoystick
 
 class ViewController: UIViewController {
+    
+//    var animator : UIDynamicAnimator? = nil
+//    var attachmentBehavior: UIAttachmentBehavior!
+//    var pushBehavior: UIPushBehavior!
+//    var itemBehavior: UIDynamicItemBehavior!
+//    var gravity = UIGravityBehavior()
+    
+    var box : UIView?
+    
+    func addBox(location: CGRect) {
+        let newBox = UIView(frame: location)
+        newBox.backgroundColor = UIColor.redColor()
+        
+        view.insertSubview(newBox, atIndex: 0)
+        box = newBox
+    }
+ 
+    
+    var animator:UIDynamicAnimator? = nil
+    let gravity = UIGravityBehavior()
+    
+    func createAnimatorStuff() {
+        animator = UIDynamicAnimator(referenceView:self.view)
+//        animator?.addBehavior(collider)
+        
+        gravity.addItem(box!)
+        gravity.gravityDirection = CGVectorMake(0, 0.8)
+        animator?.addBehavior(gravity)
+        
+        addBox(CGRectMake(100, 100, 30, 30))
+        createAnimatorStuff()
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+//        
+        
+        addBox(CGRectMake(100, 100, 30, 30))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+//        animator = UIDynamicAnimator(referenceView:self.view);
+//
+//
+//        let ballView = UIView(frame: CGRectMake(100, 200, 100, 100))
+//        ballView.backgroundColor = UIColor.greenColor()
+//        ballView.layer.cornerRadius = ballView.frame.size.width/2
+//        ballView.clipsToBounds = true
+//        ballView.layer.borderColor = UIColor.whiteColor().CGColor
+//        ballView.layer.borderWidth = 5.0
+//    
+//        gravity.addItem(ballView)
+//        gravity.gravityDirection = CGVectorMake(0, 0.8)
+//        animator?.addBehavior(gravity)
+//        
+//        
+//        
+//        self.view.addSubview(ballView)
+        
     }
 
 
