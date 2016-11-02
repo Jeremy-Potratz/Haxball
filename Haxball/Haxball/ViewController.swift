9178ec9c9f863c9521999ef195cfd2c6032c8a5e
Jotto0118 <jpotratz7338@stu.d214.org>
2016-11-02 13:55:15 -0500

    xdfsdf

--- Haxball/Haxball/ViewController.swift
+++ Haxball/Haxball/ViewController.swift
@@ -11,67 +11,91 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var animator : UIDynamicAnimator? = nil
-//    var attachmentBehavior: UIAttachmentBehavior!
-//    var pushBehavior: UIPushBehavior!
-//    var itemBehavior: UIDynamicItemBehavior!
-//    var gravity = UIGravityBehavior()
-    
-    var box : UIView?
-    
-    func addBox(location: CGRect) {
-        let newBox = UIView(frame: location)
-        newBox.backgroundColor = UIColor.redColor()
-        
-        view.insertSubview(newBox, atIndex: 0)
-        box = newBox
-    }
- 
-    
-    var animator:UIDynamicAnimator? = nil
-    let gravity = UIGravityBehavior()
-    
-    func createAnimatorStuff() {
-        animator = UIDynamicAnimator(referenceView:self.view)
-//        animator?.addBehavior(collider)
-        
-        gravity.addItem(box!)
-        gravity.gravityDirection = CGVectorMake(0, 0.8)
-        animator?.addBehavior(gravity)
-        
-        addBox(CGRectMake(100, 100, 30, 30))
-        createAnimatorStuff()
-        
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        
         
-        addBox(CGRectMake(100, 100, 30, 30))
+        
 
-        
-        
-//        animator = UIDynamicAnimator(referenceView:self.view);
-//
-//
-//        let ballView = UIView(frame: CGRectMake(100, 200, 100, 100))
-//        ballView.backgroundColor = UIColor.greenColor()
-//        ballView.layer.cornerRadius = ballView.frame.size.width/2
-//        ballView.clipsToBounds = true
-//        ballView.layer.borderColor = UIColor.whiteColor().CGColor
-//        ballView.layer.borderWidth = 5.0
-//    
-//        gravity.addItem(ballView)
-//        gravity.gravityDirection = CGVectorMake(0, 0.8)
-//        animator?.addBehavior(gravity)
-//        
-//        
-//        
-//        self.view.addSubview(ballView)
         
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        itemBehaviour.addItem(ballView)
+//
+//animator = UIDynamicAnimator(referenceView: self.view)
+//
+//gravity.addItem(ballView)
+//
+//gravity.magnitude = 0.0
+//
+//
+//
+//ballView.backgroundColor = UIColor.greenColor()
+//ballView.layer.cornerRadius = ballView.frame.size.width/2
+//ballView.clipsToBounds = true
+//ballView.layer.borderColor = UIColor.whiteColor().CGColor
+//ballView.layer.borderWidth = 5.0
+//
+//
+//
+//view.addSubview(ballView)
+//
+//collision.addItem(ballView)
+//collision.translatesReferenceBoundsIntoBoundary = true
+//
+//push.addItem(ballView)
+//push.pushDirection = vector
+//
+//
+//
+//itemBehaviour.elasticity = 1.2
+//itemBehaviour.density = 0.0
+//itemBehaviour.friction = 0.0
+//animator?.addBehavior(itemBehaviour)
+//animator?.addBehavior(collision)
+//animator?.addBehavior(push)
+//animator?.addBehavior(gravity)
+//
+//    let ballView = UIView(frame: CGRectMake(100, 200, 100, 100))
+
+var animator: UIDynamicAnimator?
+let collision = UICollisionBehavior()
+let push = UIPushBehavior()
+let gravity = UIGravityBehavior()
+let vector = CGVectorMake(1.0, 1.0)
+
+let itemBehaviour = UIDynamicItemBehavior()
+//
+
 
