2ed802df2a65ad60eb386f8692cf04d62b3f98c7
Jotto0118 <jpotratz7338@stu.d214.org>
2016-11-14 13:53:58 -0600

    added ai stuff

---
+++ Haxball/Haxball/offenseAI.swift
@@ -0,0 +1,23 @@
+//
+//  offenseAI.swift
+//  Haxball
+//
+//  Created by apcsp on 11/14/16.
+//  Copyright © 2016 potragatetz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var hello : String!
+
+class offenseAI{
+    
+    var hello = midfieldAI.myVariable.hi
+    
+    
+    //     be offensive set kick rate to high? or low? stick v close to the ball
+
+    
+    
+}
