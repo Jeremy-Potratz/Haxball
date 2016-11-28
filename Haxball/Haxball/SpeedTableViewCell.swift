5423b6627e80b2f7293a463a9acd57bb2c3468a9
Jotto0118 <jpotratz7338@stu.d214.org>
2016-11-28 13:42:58 -0600

    i don't remember shdsofsod

---
+++ Haxball/Haxball/SpeedTableViewCell.swift
@@ -0,0 +1,28 @@
+
+//
+//  SpeedTableViewCell.swift
+//  Haxball
+//
+//  Created by apcsp on 11/21/16.
+//  Copyright © 2016 potragatetz. All rights reserved.
+//
+
+import UIKit
+
+class SpeedTableViewCell: UITableViewCell {
+    
+    var SpeedArray = [1,2,3,4,5,6,7,8,9,10]
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Haxball/Haxball/backGroundCell.swift
@@ -0,0 +1,9 @@
+//
+//  backGroundCell.swift
+//  Haxball
+//
+//  Created by apcsp on 11/21/16.
+//  Copyright © 2016 potragatetz. All rights reserved.
+//
+
+import Foundation
